3367d4f7dbdde9b01a954a2cecebb4c66672279c
Joe Oketch <wokirijoe@gmail.com>
2021-07-03 20:07:00 +0300

    Initialized

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+
+
+app_name = 'users'
+
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login_page'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout_page'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+]
