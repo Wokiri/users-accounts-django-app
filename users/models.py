2a058b578bcbc488a4ac9fd59926140855fee813
Joe Oketch Wokiri <wokirijoe@gmail.com>
2021-08-25 10:40:06 +0300

    Case 1: Extending the attributes of django's default User

--- users/models.py
+++ users/models.py
@@ -1,79 +1,23 @@
-  
 from django.db import models
-from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser
-
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
 
+class LibraryUser(models.Model):
+
+    class Sections(models.TextChoices):
+        ADVENTURE = 'ADV', _('Adventure')
+        REALITY = 'REAL', _('Real')
+        EDUCATION = 'EDU', _('Education')
 
 
-import uuid
-
-
-
-
-# phone_regex = r'\d.+{13}'
-
-
-class CustomUserManager(BaseUserManager):
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
-    def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a User with the given email and password.
-        """
-        if not email:
-            raise ValueError(_('The Email must be set'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, password, **extra_fields)
-
-
-
-
-class User(AbstractUser):
-    id = models.UUIDField(
-        primary_key=True,
-        unique=True,
-        default=uuid.uuid4,
-        editable=False,
-    )
-
-    name = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=13, unique=True)
-    email = models.EmailField(_('email address'), unique=True)
-    password = models.CharField(max_length=255)
-    
-    date_joined = models.DateField(auto_now_add=True)
-    
-    username = None
-
-    objects = CustomUserManager()
-
-
-    REQUIRED_FIELDS = ('name', 'phone_number',)
-    USERNAME_FIELD = 'email'
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_section = models.CharField(_('favorite section'), max_length=5, choices=Sections.choices)
+    is_administrator = models.BooleanField(default=False)
+    is_librarian = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=True)
 
 
 
     def __str__(self):
-        return (f'{self.name} ({self.email})')
+        return (f'{self.user.first_name} ({self.user.email})')
